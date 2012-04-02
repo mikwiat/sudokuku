0a2bc6ddde0cb4ad42ee1f5e1baf095fffb5c32f
Peter Bergström <petb@redpill-linpro.com>
2012-04-02 16:18:29 +0200

    New index.php with skeleton

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
 
-	echo 'Magic!';
+if ($_POST)
+{
+	try
+	{
+		$fileUploader = new FileUploader($_FILES['sudoku_image']);
+		$fileUploader->doUpload();
+		$image = $fileUploader->getImage();
+	
+		$imageParser = new ImageParser($image);
+		$imageParser->parse();
+		$numbers = $imageParser->getNumbers();
 
+		$sudoku = new Sudoku();
+		foreach ($numbers as $number)
+		{
+			$sudoku->addNumber($number);
+		}
+		$sudoku->solve();
+	}
+	catch (Exception $e)
+	{
+		// Present error msg
+		die();
+	}
+	$sudoku->display();
+}
+
+?>
+<html> blablabla </html>
